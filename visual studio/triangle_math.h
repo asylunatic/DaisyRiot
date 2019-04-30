a369af5e802ce4df778f49dddd3d36a410afb65d
Marijn Roelvink <m.a.t.roelvink@student.tudelft.nl>
2019-04-30 15:46:30 +0200

    Add reciprocity formula back in matrix filling

--- visual studio/triangle_math.h
+++ visual studio/triangle_math.h
@@ -9,6 +9,7 @@ namespace triangle_math
 	glm::vec3 calculateCentre(std::vector<glm::vec3> triangle);
 	float calculateSurface(std::vector<glm::vec3> triangle);
 	float calculateSurface(glm::vec3 a, glm::vec3 b, glm::vec3 c);
+	float calculateSurface(int triangleId, std::vector<Vertex> &vertices);
 	bool isFacingBack(glm::vec3 origin, float destPatch, std::vector<Vertex> &vertices);
 	glm::vec3 avgNormal(float triangleId, std::vector<Vertex> &vertices);
 	float calcPointFormfactor(Vertex orig, Vertex dest, float surface);
