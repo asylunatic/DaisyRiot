9a00c3f10c3b3a1dea5241b01f970fd79458a0d2
Merge Conflicts <omdatjehetwaardbent@hotmail.com>
2019-06-08 22:26:32 +0200

    Add the possibility to sample spectrum from a given set of wavelengths

--- visual studio/Lightning.h
+++ visual studio/Lightning.h
@@ -9,6 +9,7 @@ public:
 	virtual void converge_lightning() = 0;
 	virtual void increment_lightpass() = 0;
 	virtual void reset() = 0;
+
 protected:
 	int numpasses;
 	SpMat RadMat;
@@ -67,39 +68,12 @@ protected:
 			readFile.close();
 		}
 	}
-};
-
-class RGBLightning : public Lightning{
-public:
-	float emission_value;
-	std::vector<Eigen::VectorXf> emission;
-	int numsamples = 3;
-	std::vector<Eigen::VectorXf> residualvector;
-	std::vector<Eigen::VectorXf> lightningvalues; // per color channel
-	std::vector<Eigen::VectorXf> reflectionvalues; // how much of which color is reflected per patch
-
-	int numpasses;
-	SpMat RadMat;
-
-	RGBLightning(MeshS &mesh, OptixPrimeFunctionality &optixP, float &emissionval){
-		emission_value = emissionval;
-		set_sampled_emission(mesh);
-		set_reflectionvalues(mesh);
-
+	void initMat(MeshS &mesh, OptixPrimeFunctionality &optixP){
 		RadMat = SpMat(mesh.numtriangles, mesh.numtriangles);
-		optixP.calculateRadiosityMatrix(RadMat, mesh);
-
-		reset();
-		converge_lightning();
+		optixP.calculateRadiosityMatrix(RadMat, mesh); 
 	}
-
-	RGBLightning(MeshS &mesh, OptixPrimeFunctionality &optixP, float &emissionval, char* matfile){
-		emission_value = emissionval;
-		set_sampled_emission(mesh);
-		set_reflectionvalues(mesh);
-
+	void initMatFromFile( MeshS &mesh, OptixPrimeFunctionality &optixP, char* matfile){
 		RadMat = SpMat(mesh.numtriangles, mesh.numtriangles);
-
 		bool fileExists = ImageExporter::exists(matfile);
 		if (fileExists){
 			DeserializeMat(RadMat, matfile);
@@ -110,7 +84,116 @@ public:
 			SerializeMat(RadMat, matfile);
 			std::cout << "Loaded & Serialized matrix" << std::endl;
 		}
-		
+	}
+};
+
+class SpectralLightning : public Lightning{
+private:
+	float emission_value;
+	int numsamples;
+	std::vector<Eigen::VectorXf> emission;
+	std::vector<Eigen::VectorXf> residualvector;
+	std::vector<Eigen::VectorXf> lightningvalues; // per color channel
+	std::vector<Eigen::VectorXf> reflectionvalues; // how much of which color is reflected per patch
+
+public:
+	SpectralLightning(MeshS &mesh, OptixPrimeFunctionality &optixP, float &emissionval, int num_samples){
+		numsamples = num_samples;
+		emission_value = emissionval;
+		set_sampled_emission(mesh);
+		set_reflectionvalues(mesh);
+		initMat(mesh, optixP);
+		reset();
+		converge_lightning();
+	}
+
+	SpectralLightning(MeshS &mesh, OptixPrimeFunctionality &optixP, float &emissionval, char* matfile){
+		emission_value = emissionval;
+		set_sampled_emission(mesh);
+		set_reflectionvalues(mesh);
+		initMatFromFile(mesh, optixP, matfile);
+		reset();
+		converge_lightning();
+	}
+
+	glm::vec3 get_color_of_patch(int index){
+		return{ lightningvalues[0][index], lightningvalues[1][index], lightningvalues[2][index] };
+	}
+
+	void converge_lightning(){
+		while (residualvector[0].sum() > 0.0001 || residualvector[1].sum() > 0.0001 || residualvector[2].sum() > 0.0001){
+			increment_lightpass();
+		}
+	}
+
+	void increment_lightpass(){
+		for (int i = 0; i < numsamples; i++){
+			residualvector[i] = (RadMat * residualvector[i]).cwiseProduct(reflectionvalues[i]);
+			lightningvalues[i] = lightningvalues[i] + residualvector[i];
+		}
+		numpasses++;
+	}
+
+	void reset(){
+		residualvector = emission;
+		lightningvalues = emission;
+		numpasses = 0;
+	}
+
+private:
+
+	void set_sampled_emission(MeshS &mesh){
+		emission.resize(numsamples);
+		for (int i = 0; i < numsamples; i++){
+			emission[i] = Eigen::VectorXf::Zero(mesh.numtriangles);
+			// set emissive values from material
+			for (int j = 0; j < mesh.numtriangles; j++){
+				if (mesh.materials[mesh.materialIndexPerTriangle[j]].emission[i] > 0.0){
+					emission[i][j] = mesh.materials[mesh.materialIndexPerTriangle[j]].emission[i] * emission_value;
+				}
+			}
+		}
+	}
+
+	void set_reflectionvalues(MeshS &mesh){
+		reflectionvalues.resize(numsamples);
+		for (int i = 0; i < numsamples; i++){
+			reflectionvalues[i] = Eigen::VectorXf::Zero(mesh.numtriangles);
+			for (int j = 0; j < mesh.numtriangles; j++){
+				if (mesh.materials[mesh.materialIndexPerTriangle[j]].rgbcolor[i] > 0.0){
+					reflectionvalues[i](j) = mesh.materials[mesh.materialIndexPerTriangle[j]].rgbcolor[i];
+				}
+			}
+		}
+	}
+
+
+};
+
+class RGBLightning : public Lightning{
+private:
+	float emission_value;
+	int numsamples = 3;
+	std::vector<Eigen::VectorXf> emission;
+	std::vector<Eigen::VectorXf> residualvector;
+	std::vector<Eigen::VectorXf> lightningvalues; // per color channel
+	std::vector<Eigen::VectorXf> reflectionvalues; // how much of which color is reflected per patch
+
+public:
+	RGBLightning(MeshS &mesh, OptixPrimeFunctionality &optixP, float &emissionval){
+		emission_value = emissionval;
+		set_sampled_emission(mesh);
+		set_reflectionvalues(mesh);
+		initMat(mesh, optixP);
+		reset();
+		converge_lightning();
+	}
+
+	RGBLightning(MeshS &mesh, OptixPrimeFunctionality &optixP, float &emissionval, char* matfile){
+		emission_value = emissionval;
+		set_sampled_emission(mesh);
+		set_reflectionvalues(mesh);
+		initMatFromFile(mesh, optixP, matfile);
 		reset();
 		converge_lightning();
 	}
@@ -177,19 +260,9 @@ public:
 	
 	BWLightning(MeshS &mesh, OptixPrimeFunctionality &optixP, float &emissionval){
 		emission_value = emissionval;
-
 		setemission(mesh);
-
-		// initialize radiosity matrix
-		RadMat = SpMat(mesh.numtriangles, mesh.numtriangles);
-		optixP.calculateRadiosityMatrixStochastic(RadMat, mesh);
-
-		lightningvalues = emission;
-		residualvector = emission;
-		// first lightning
-		numpasses = 0;
-
-		// converge lightning
+		initMat(mesh, optixP);
+		reset();
 		converge_lightning();
 	}
 
--- visual studio/Material.h
+++ visual studio/Material.h
@@ -6,36 +6,66 @@
 
 class Material{
 public:
-	glm::vec3 rgbcolor; // will treat as if constains wavelengths of 400 500 and 650 nm (rougly rgb)
-	glm::vec3 rgbspectralcolor;
-	glm::vec3 xyzcolor;
+	glm::vec3 rgbcolor; 
 	glm::vec3 emission;
+	int numwavelengths;
+
+	glm::vec3 rgbspectralcolor;
+
+	std::vector<float> spectral_values;
+	//std::vector<float> spectral_emission; // not used yet
+	std::vector<float> wavelengths;
 	
-	Material(glm::vec3 rgbcolor, glm::vec3 emission) :rgbcolor(rgbcolor), emission(emission){
-		// set spectralcolorfor rgb
+	Material(glm::vec3 rgbcolor, glm::vec3 emission, std::vector<float> wave_lengths) :rgbcolor(rgbcolor), emission(emission), wavelengths(wave_lengths){
+		numwavelengths = wave_lengths.size();
+		// set spectrum from color rgb
 		RGB2Spec *model = rgb2spec_load("tables/srgb.coeff");
 		float rgb[3] = { rgbcolor.x, rgbcolor.y, rgbcolor.z }, coeff[3];
 		rgb2spec_fetch(model, rgb, coeff);
 
+		for (int i = 0; i < numwavelengths; i++){
+			float wavelength_from_rgb = rgb2spec_eval_precise(coeff, wave_lengths[i]);
+			spectral_values.push_back(wavelength_from_rgb);
+		}
+
+		// init rgbspectralcolor
+		rgb2spec_fetch(model, rgb, coeff);
 		float sample_600 = rgb2spec_eval_precise(coeff, 600);
 		float sample_550 = rgb2spec_eval_precise(coeff, 550);
 		float sample_450 = rgb2spec_eval_precise(coeff, 450);
 
 		rgbspectralcolor = { sample_600, sample_550, sample_450 };
 
+		// debug prints
+		std::cout << "rgbspectralcolor, 600 = " << sample_600 << ", 550 = " << sample_550 << ", 450 = " << sample_450 << std::endl;
+		std::cout << "now print from spectral_values: " << std::endl;
+		for (int i = 0; i < numwavelengths; i++){
+			std::cout << "wavelength is " << wave_lengths[i] << " spectral value is = " << spectral_values[i] << std::endl;
+		}
+
 		rgb2spec_free(model);
 	};
 	
 	Material(){
 		rgbcolor = glm::vec3();
-		xyzcolor = glm::vec3();
 		emission = glm::vec3();
 	};
 
-	glm::vec3 get_color_from_spectrum(){
+	glm::vec3 get_color_from_rgbspectrum(){
 		glm::vec3 xyz = cie1931WavelengthToXYZFit(600) * rgbspectralcolor[0]
-						+ cie1931WavelengthToXYZFit(550) * rgbspectralcolor[1]
-						+ cie1931WavelengthToXYZFit(460) * rgbspectralcolor[2];
+				+ cie1931WavelengthToXYZFit(550) * rgbspectralcolor[1]
+				+ cie1931WavelengthToXYZFit(450) * rgbspectralcolor[2];
+		glm::vec3 rgb;
+		XYZToRGB(xyz, rgb);
+		return rgb;
+	}
+
+
+	glm::vec3 get_color_from_spectrum(){
+		glm::vec3 xyz = { 0.0, 0.0, 0.0 };
+		for (int i = 0; i < numwavelengths; i++){
+			xyz += cie1931WavelengthToXYZFit(wavelengths[i]) * spectral_values[i];
+		}
 		glm::vec3 rgb;
 		XYZToRGB(xyz, rgb);
 		return rgb;
